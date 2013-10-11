895a60d4c8eb8bc72fae993eb800247f7c0b67cd
Nate Wright <natewr@users.sourceforge.net>
2013-10-11 16:06:27 +0100

    * Add blank option support to Post and Taxonomy select fields

--- classes/AdminPageSetting.SelectPost.class.php
+++ classes/AdminPageSetting.SelectPost.class.php
@@ -11,6 +11,9 @@ class sapAdminPageSettingSelectPost extends sapAdminPageSetting {
 
 	public $sanitize_callback = 'sanitize_text_field';
 
+	// Whether or not to display a blank option
+	public $blank_option = true;
+
 	/**
 	 * An array of arguments accepted by get_posts().
 	 * See: http://codex.wordpress.org/Template_Tags/get_posts
@@ -21,11 +24,12 @@ class sapAdminPageSettingSelectPost extends sapAdminPageSetting {
 	 * Initialize the setting
 	 * @since 1.0
 	 */
-	public function __construct( $id, $title, $description, $args = array() ) {
+	public function __construct( $id, $title, $description, $args = array(), $blank_option = true ) {
 
 		$this->id = esc_attr( $id );
 		$this->title = $title;
 		$this->description = $description;
+		$this->blank_option = $blank_option;
 		$this->args = $args;
 		$this->value = $this->esc_value( get_option ( $this->id ) );
 
@@ -42,9 +46,15 @@ class sapAdminPageSettingSelectPost extends sapAdminPageSetting {
 		?>
 
 			<select name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>">
+
+				<?php if ( $this->blank_option === true ) : ?>
+					<option></option>
+				<?php endif; ?>
+
 				<?php foreach ( $posts as $post  ) : ?>
 					<option value="<?php echo esc_attr( $post->ID ); ?>"<?php if( $this->value == $post->ID ) : ?> selected="selected"<?php endif; ?>><?php echo esc_html( $post->post_title ); ?></option>
 				<?php endforeach; ?>
+
 			</select>
 
 		<?php
--- classes/AdminPageSetting.SelectTaxonomy.class.php
+++ classes/AdminPageSetting.SelectTaxonomy.class.php
@@ -11,25 +11,31 @@
 class sapAdminPageSettingSelectTaxonomy extends sapAdminPageSetting {
 
 	public $sanitize_callback = 'sanitize_text_field';
+	
+	// Whether or not to display a blank option
+	public $blank_option = true;
 
+	// Arrays of taxonomies to fetch (required)
+	public $taxonomies;
+	
 	/**
-	 * Arrays of taxonomies and arguments accepted by get_terms().
+	 * Array of options accepted by get_terms()
 	 * See: http://codex.wordpress.org/Function_Reference/get_terms
 	 */
-	public $taxonomies = array();
 	public $args = array();
 
 	/**
 	 * Initialize the setting
 	 * @since 1.0
 	 */
-	public function __construct( $id, $title, $description, $taxonomies, $args = array() ) {
+	public function __construct( $id, $title, $description, $taxonomies, $blank_option = true, $args = array() ) {
 
 		$this->id = esc_attr( $id );
 		$this->title = $title;
 		$this->description = $description;
 		$this->description = $description;
 		$this->taxonomies = $taxonomies;
+		$this->blank_option = $blank_option;
 		$this->args = $args;
 		$this->value = $this->esc_value( get_option ( $this->id ) );
 
@@ -46,9 +52,15 @@ class sapAdminPageSettingSelectTaxonomy extends sapAdminPageSetting {
 		?>
 
 			<select name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>">
+			
+				<?php if ( $this->blank_option === true ) : ?>
+					<option></option>
+				<?php endif; ?>
+				
 				<?php foreach ( $terms as $term  ) : ?>
 					<option value="<?php echo esc_attr( $term->term_id ); ?>"<?php if( $this->value == $term->term_id ) : ?> selected="selected"<?php endif; ?>><?php echo esc_html( $term->name ); ?></option>
 				<?php endforeach; ?>
+				
 			</select>
 
 		<?php
