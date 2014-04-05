NateWr/simple-admin-pages

commit 2c9c94750d2602b5563ea83cdc0e072fca430844

* Implement new version handling

simple-admin-pages.php
@@ -12,51 +12,41 @@
  *
  * @since 1.0
  * @package Simple Admin Pages
- *
- * @todo license
+ * @license GNU GPL 2 or later
  * @todo readme
  */
 
-// Only load the library if it hasn't already been loaded. This will prevent#
-// clashes if multiple plugins using the same library.
-if ( !defined( 'SAP_TEXTDOMAIN' ) ) {
+/**
+ * Initialize the appropriate version of the libary.
+ *
+ * This function should remain backwards compatible at all times, so that the
+ * initialization function from version 1.0 will still be able to initialize
+ * the library appropriately for version 2.0. This way, if two plugins exist
+ * with different versions, the plugin creator will still be able to initialize
+ * their library.
+ *
+ * @since 1.0
+ */
+if ( !function_exists( 'sap_initialize_library' ) ) {
 
-	// Set a textdomain for translation
-	define( 'SAP_TEXTDOMAIN', 'sapdomain' );
+	function sap_initialize_library( $args = array() ) {
 	
-	// Register the version of the library. This is not the public version
-	// number (ie - 1.0), but an internal integer we can compare easily to
-	// determine if an out-of-date version of the library is being used.
-	define( 'SAP_VERSION', 1); // @version
+		// Exit early if no version was provided
+		if ( !isset( $args['version'] ) ) {
+			return null;
+		}
 
-	// Make sure we have access to WordPress's plugin functions
-	require_once(ABSPATH . '/wp-admin/includes/plugin.php');
+		// Load the library
+		require_once('classes/Library.class.php');
 
-	// Load the library's classes
-	require_once('classes/AdminPage.class.php');
+		// Set the textdomain for translation
+		if ( !defined( 'SAP_TEXTDOMAIN' ) ) {
+			define( 'SAP_TEXTDOMAIN', 'sapdomain' );
+		}
 
-} else {
+		$lib_class_name = 'sapLibrary_' . str_replace( '.', '_', $args['version'] );
 
-	// Compare the versions to see if they need to be bumped.
-	if ( SAP_VERSION < 1 ) { // @version
-		// @todo I should display a notice that informs the user that an older
-		// version of this library is being used.
-		
-		// @todo Maybe there is a better way. Maybe an abstract class like
-		// sapLibrary which each plugin uses to declare the version they're
-		// built against. Then the classes will only be declared if their
-		// version matches the sapLibrary version. Something like:
-		// $lib = new sapLibrary( '1.0' );
-		// $lib->setup(); // loads necessary class files
-		// 
-		// then in the class definitions:
-		//
-		// if ($lib->version == '1.0') {
-		// 	class sapAdminPage { }
-		// }
-		// 
-		// maybe this would mean the proper version of the classes were loaded?
-		// no i dont think it would, because each library is going to load its
-		// own on every page load anyway.
+		return new $lib_class_name( $args );
 	}
+
 }

classes/AdminPage.class.php
@@ -7,27 +7,16 @@
  * @package Simple Admin Pages
  */
 
-require_once('AdminPageSection.class.php');
-require_once('AdminPageSetting.class.php');
-require_once('AdminPageSetting.Text.class.php');
-require_once('AdminPageSetting.Textarea.class.php');
-require_once('AdminPageSetting.Select.class.php');
-require_once('AdminPageSetting.Checkbox.class.php');
-require_once('AdminPageSetting.OpeningHours.class.php');
-require_once('AdminPageSetting.SelectPost.class.php');
-require_once('AdminPageSetting.SelectTaxonomy.class.php');
-
-class sapAdminPage {
+class sapAdminPage_1_0 {
 
 	public $title;
-	public $title_menu;
+	public $menu_title;
 	public $description; // optional description for this page
 	public $capability; // user permissions needed to edit this panel
-	public $slug; // id of this page
-	public $lib_url; // url to this library
+	public $id; // id of this page
 	public $icon = 'icon-options-general';
 	public $sections = array(); // array of sections to display on this page
-	
+
 	private $section_class_name = 'sapAdminPageSection';
 
 
@@ -35,16 +24,13 @@ class sapAdminPage {
 	 * Initialize the page
 	 * @since 1.0
 	 */
-	public function __construct( $title, $title_menu, $description, $capability, $slug, $lib_url ) {
+	public function __construct( $id, $title, $menu_title, $description, $capability ) {
 
+		$this->id = esc_attr( $id ); // id of this page
 		$this->title = $title;
-		$this->title_menu = $title_menu;
+		$this->menu_title = $menu_title;
 		$this->description = $description;
 		$this->capability = $capability;
-		$this->slug = esc_attr( $slug ); // id of this page
-		$this->lib_url = esc_url( $lib_url ); // id of this page
-		
-		add_action( 'admin_print_styles', array( $this, 'enqueue_scripts' ) );
 
 	}
 
@@ -55,7 +41,7 @@ class sapAdminPage {
 	 * @since 1.0
 	 */
 	public function add_admin_menu() {
-		add_options_page( $this->title, $this->title_menu, $this->capability, $this->slug, array( $this, 'display_admin_menu' ) );
+		add_options_page( $this->title, $this->menu_title, $this->capability, $this->id, array( $this, 'display_admin_menu' ) );
 	}
 
 	/**
@@ -63,11 +49,12 @@ class sapAdminPage {
 	 * @since 1.0
 	 */
 	public function add_section( $section ) {
-		if ( !$section || !get_class( $section ) == $this->section_class_name ) {
+		if ( !$section ) {
 			return;
 		}
 
-		array_push( $this->sections, $section );
+		$this->sections[ $section->id ] = $section;
+
 	}
 
 	/**
@@ -78,11 +65,11 @@ class sapAdminPage {
 
 		// Loop over each section
 		foreach ( $this->sections as $section ) {
-			$section->add_settings_section( $this->slug );
+			$section->add_settings_section( $this->id );
 
 			// Loop over each setting
 			foreach ( $section->settings as $setting ) {
-				$setting->add_register_setting( $this->slug, $section->id );
+				$setting->add_register_setting( $this->id, $section->id );
 			}
 		}
 	}
@@ -90,8 +77,6 @@ class sapAdminPage {
 
 	/**
 	 * Output the settings passed to this page
-	 * @todo the values of the fields should probably be fetched here
-	 * @todo maybe here is where the fields should be registered as well?
 	 * @since 1.0
 	 */
 	public function display_admin_menu() {
@@ -106,8 +91,8 @@ class sapAdminPage {
 				<?php $this->display_page_title(); ?>
 
 				<form method="post" action="options.php">
-					<?php settings_fields( $this->slug ); ?>
-					<?php do_settings_sections( $this->slug ); ?>
+					<?php settings_fields( $this->id ); ?>
+					<?php do_settings_sections( $this->id ); ?>
 					<?php submit_button(); ?>
 				 </form>
 			</div>
@@ -148,15 +133,6 @@ class sapAdminPage {
 		?>
 			<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"  /></p>
 		<?php
-	}
-
-	/**
-	 * Enqueue the CSS stylesheet
-	 * @since 1.0
-	 */
-	public function enqueue_scripts() {
-		wp_enqueue_style( 'esp-admin-style', $this->lib_url . 'css/admin.css' );
-		wp_enqueue_script( 'esp-admin-script', $this->lib_url . 'js/admin.js', array( 'jquery' ), '1.0', true );
 	}
 
 }

classes/AdminPageSection.class.php
@@ -7,7 +7,7 @@
  * @package Simple Admin Pages
  */
 
-class sapAdminPageSection {
+class sapAdminPageSection_1_0 {
 
 	// Page defaults
 	public $id; // unique id for this section
@@ -15,19 +15,47 @@ class sapAdminPageSection {
 	public $description; // optional description of the section
 	public $settings = array(); // Array of settings to display in this option set
 	
-	private $setting_class_name = 'sapAdminPageSetting';
-
+	// Array to store errors
+	public $errors = array();
 
 	/**
 	 * Initialize the section
 	 * @since 1.0
 	 */
-	public function __construct( $id, $title, $description ) {
+	public function __construct( $args ) {
 
-		$this->id = esc_attr( $id );
-		$this->title = $title;
-		$this->description = $description;
+		// Parse the values passed
+		$this->parse_args( $args );
 
+		// Set an error if there is no id for this section
+		if ( !isset( $this->id ) ) {
+			$this->set_error(
+				array( 
+					'type'		=> 'missing_data',
+					'data'		=> 'id'
+				)
+			);
+		}
+
+	}
+
+	/**
+	 * Parse the arguments passed in the construction and assign them to
+	 * internal variables.
+	 * @since 1.0
+	 */
+	private function parse_args( $args ) {
+		foreach ( $args as $key => $val ) {
+			switch ( $key ) {
+
+				case 'id' :
+					$this->{$key} = esc_attr( $val );
+				
+				default :
+					$this->{$key} = $val;
+					
+			}
+		}
 	}
 	
 	/**
@@ -35,11 +63,11 @@ class sapAdminPageSection {
 	 * @since 1.0
 	 */
 	public function add_setting( $setting ) {
-		if ( !$setting || !is_subclass_of( $setting, $this->setting_class_name ) ) {
+		if ( !$setting ) {
 			return;
 		}
 		
-		array_push( $this->settings, $setting );
+		$this->settings[ $setting->id ] = $setting;
 	}
 
 	/**
@@ -67,6 +95,21 @@ class sapAdminPageSection {
 	 */
 	public function add_settings_section( $page_slug ) {
 		add_settings_section( $this->id, $this->title, array( $this, 'display_section' ), $page_slug );
+	}
+	
+	/**
+	 * Set an error
+	 * @since 1.0
+	 */
+	public function set_error( $error ) {
+		$this->errors[] = array_merge(
+			$error,
+			array(
+				'class'		=> get_class( $this ),
+				'id'		=> $this->id,
+				'backtrace'	=> debug_backtrace()
+			)
+		);
 	}
 	
 }

classes/AdminPageSetting.OpeningHours.class.php
@@ -4,11 +4,28 @@
  * Register, display and save a series of fields to specify the opening hours
  * of a business/company.
  *
+ * This setting accepts the following arguments in its constructor function.
+ *
+ * $args = array(
+ *		'id'			=> 'setting_id', 	// Unique id
+ *		'title'			=> 'My Setting', 	// Title or label for the setting
+ *		'description'	=> 'Description', 	// Help text description
+ *		'weekday_names'	=> array(			// Optional array of custom 
+ *			'monday'		=> 'Monday',	//	weekday names. These can be
+ *			'tuesday'		=> 'Tuesday',   //	passed in any order to
+ *			'wednesday'		=> 'Wednesday',	//	set a new start of the week.
+ *			'thursday'		=> 'Thursday',
+ *			'friday'		=> 'Friday',
+ *			'saturday'		=> 'Saturday',
+ *			'sunday'		=> 'Sunday'
+ *		);
+ * );
+ *
  * @since 1.0
  * @package Simple Admin Pages
  */
 
-class sapAdminPageSettingOpeningHours extends sapAdminPageSetting {
+class sapAdminPageSettingOpeningHours_1_0 extends sapAdminPageSetting_1_0 {
 
 	public $sanitize_callback = 'sanitize_text_field';
 
@@ -24,22 +41,33 @@ class sapAdminPageSettingOpeningHours extends sapAdminPageSetting {
 	);
 
 	/**
-	 * Initialize the setting
+	 * Parse the arguments passed in the construction and assign them to
+	 * internal variables.
 	 * @since 1.0
 	 */
-	public function __construct( $id, $title, $description, $weekday_names = array() ) {
+	private function parse_args( $args ) {
+		foreach ( $args as $key => $val ) {
+			switch ( $key ) {
 
-		$this->id = esc_attr( $id );
-		$this->title = $title;
-		$this->description = $description;
-		$this->value = $this->esc_value( get_option( $this->id ) );
+				case 'id' :
+					$this->{$key} = esc_attr( $val );
 
-		// Allow weekday names to be overwritten for easy translation
-		if ( is_array( $weekday_names ) && count( $weekday_names ) ) {
-			foreach ( $weekday_names as $id => $name ) {
-				if ( isset( $this->weekdays[$id] ) ) {
-					$this->weekdays[$id] = $name;
-				}
+				case 'weekdays' :
+				
+					$this->weekdays = $val;
+
+					// Allow weekday names to be overwritten for easy translation
+//					if ( is_array( $val ) && count( $val ) ) {
+//						foreach ( $val as $id => $name ) {
+//							if ( isset( $this->weekdays[$id] ) ) {
+//								$this->weekdays[$id] = $name;
+//							}
+//						}
+//					}
+
+				default :
+					$this->{$key} = $val;
+
 			}
 		}
 	}

classes/AdminPageSetting.Select.class.php
@@ -3,38 +3,33 @@
 /**
  * Register, display and save a selection option with a drop-down menu.
  *
+ * This setting accepts the following arguments in its constructor function.
+ *
+ * $args = array(
+ *		'id'			=> 'setting_id', 	// Unique id
+ *		'title'			=> 'My Setting', 	// Title or label for the setting
+ *		'description'	=> 'Description', 	// Help text description
+ *		'blank_option'	=> true, 			// Whether or not to show a blank option
+ *		'options'		=> array(			// An array of key/value pairs which
+ *			'option1'	=> 'Option 1',		//	define the options.
+ *			'option2'	=> 'Option 2',
+ *			...
+ *		);
+ * );
+ *
  * @since 1.0
  * @package Simple Admin Pages
  */
 
-class sapAdminPageSettingSelect extends sapAdminPageSetting {
+class sapAdminPageSettingSelect_1_0 extends sapAdminPageSetting_1_0 {
 
 	public $sanitize_callback = 'sanitize_text_field';
 
 	// Whether or not to display a blank option
 	public $blank_option = true;
 
-	/**
-	 * An array of options for this select field, accepted as a key/value pair.
-	 * The key is the option value and the value is the text displayed to the
-	 * user.
-	 */
+	// An array of options for this select field, accepted as a key/value pair.
 	public $options = array();
-
-	/**
-	 * Initialize the setting
-	 * @since 1.0
-	 */
-	public function __construct( $id, $title, $description, $options, $blank_option = true ) {
-
-		$this->id = esc_attr( $id );
-		$this->title = $title;
-		$this->description = $description;
-		$this->blank_option = $blank_option;
-		$this->options = $options;
-		$this->value = $this->esc_value( get_option ( $this->id ) );
-
-	}
 
 	/**
 	 * Display this setting

classes/AdminPageSetting.SelectPost.class.php
@@ -3,11 +3,21 @@
 /**
  * Register, display and save a selection with a drop-down list of any post type
  *
+ * This setting accepts the following arguments in its constructor function.
+ *
+ * $args = array(
+ *		'id'			=> 'setting_id', 	// Unique id
+ *		'title'			=> 'My Setting', 	// Title or label for the setting
+ *		'description'	=> 'Description', 	// Help text description
+ *		'blank_option'	=> true, 			// Whether or not to show a blank option
+ *		'args'			=> array();			// Arguments to pass to WordPress's get_post() function
+ * );
+ *
  * @since 1.0
  * @package Simple Admin Pages
  */
 
-class sapAdminPageSettingSelectPost extends sapAdminPageSetting {
+class sapAdminPageSettingSelectPost_1_0 extends sapAdminPageSetting_1_0 {
 
 	public $sanitize_callback = 'sanitize_text_field';
 
@@ -19,21 +29,6 @@ class sapAdminPageSettingSelectPost extends sapAdminPageSetting {
 	 * See: http://codex.wordpress.org/Template_Tags/get_posts
 	 */
 	public $args = array();
-
-	/**
-	 * Initialize the setting
-	 * @since 1.0
-	 */
-	public function __construct( $id, $title, $description, $args = array(), $blank_option = true ) {
-
-		$this->id = esc_attr( $id );
-		$this->title = $title;
-		$this->description = $description;
-		$this->blank_option = $blank_option;
-		$this->args = $args;
-		$this->value = $this->esc_value( get_option ( $this->id ) );
-
-	}
 
 	/**
 	 * Display this setting

classes/AdminPageSetting.SelectTaxonomy.class.php
@@ -2,13 +2,24 @@
 
 /**
  * Register, display and save a selection with a drop-down list of any taxonomy
+ *
+ * This setting accepts the following arguments in its constructor function.
+ *
+ * $args = array(
+ *		'id'			=> 'setting_id', 	// Unique id
+ *		'title'			=> 'My Setting', 	// Title or label for the setting
+ *		'description'	=> 'Description', 	// Help text description
+ *		'taxonomies'	=> array();			// Array of taxonomies to fetch (required)
+ *		'blank_option'	=> true, 			// Whether or not to show a blank option
+ *		'args'			=> array();			// Arguments to pass to WordPress's get_terms() function
+ * );
  * type
  *
  * @since 1.0
  * @package Simple Admin Pages
  */
 
-class sapAdminPageSettingSelectTaxonomy extends sapAdminPageSetting {
+class sapAdminPageSettingSelectTaxonomy_1_0 extends sapAdminPageSetting_1_0 {
 
 	public $sanitize_callback = 'sanitize_text_field';
 	
@@ -23,23 +34,6 @@ class sapAdminPageSettingSelectTaxonomy extends sapAdminPageSetting {
 	 * See: http://codex.wordpress.org/Function_Reference/get_terms
 	 */
 	public $args = array();
-
-	/**
-	 * Initialize the setting
-	 * @since 1.0
-	 */
-	public function __construct( $id, $title, $description, $taxonomies, $blank_option = true, $args = array() ) {
-
-		$this->id = esc_attr( $id );
-		$this->title = $title;
-		$this->description = $description;
-		$this->description = $description;
-		$this->taxonomies = $taxonomies;
-		$this->blank_option = $blank_option;
-		$this->args = $args;
-		$this->value = $this->esc_value( get_option ( $this->id ) );
-
-	}
 
 	/**
 	 * Display this setting

classes/AdminPageSetting.Text.class.php
@@ -7,7 +7,7 @@
  * @package Simple Admin Pages
  */
 
-class sapAdminPageSettingText extends sapAdminPageSetting {
+class sapAdminPageSettingText_1_0 extends sapAdminPageSetting_1_0 {
 
 	public $sanitize_callback = 'sanitize_text_field';
 

commit c01ffb8ec0a8d97f8ee40b0bcec0d2959b861669

* Fix: always load library files before function check
This fixes a critical error in the version handling technique introduced in 2c9c94750d2602b5563ea83cdc0e072fca430844. Because the class files were only loaded by the first sap_initialize_library() function that was defined, multiple active plugins using different versions would lead to cases where the correct version of the library could not be instantiated.

simple-admin-pages.php
@@ -16,6 +16,15 @@
  */
 
 /**
+ * Always load the library files attached to this copy of SAP
+ *
+ * This fixes a compatibility bug if two versions of the library are being
+ * loaded by different plugins/themes. However, versions prior to 2.0 do not
+ * include this and can cause compatibility issues.
+ */
+require_once( 'classes/Library.class.php' );
+
+/**
  * Initialize the appropriate version of the libary.
  *
  * This function should remain backwards compatible at all times, so that the
@@ -34,9 +43,6 @@ if ( !function_exists( 'sap_initialize_library' ) ) {
 		if ( !isset( $args['version'] ) ) {
 			return null;
 		}
-
-		// Load the library
-		require_once( 'classes/Library.class.php' );
 
 		// Set the textdomain for translation
 		if ( !defined( 'SAP_TEXTDOMAIN' ) ) {