e32c42d097681575a6bc66eccb2f9bd0121ca6c9
Nate Wright <natew@notthisway.com>
2014-08-20 13:34:26 +0100

    * Version 2.0.a.7

--- classes/Library.class.php
+++ classes/Library.class.php
@@ -1,5 +1,5 @@
 <?php
-if ( !class_exists( 'sapLibrary_2_0_a_6' ) ) {
+if ( !class_exists( 'sapLibrary_2_0_a_7' ) ) {
 /**
  * This library class loads and provides access to the correct version of the
  * Simple Admin Pages library.
@@ -7,10 +7,10 @@ if ( !class_exists( 'sapLibrary_2_0_a_6' ) ) {
  * @since 1.0
  * @package Simple Admin Pages
  */
-class sapLibrary_2_0_a_6 {
+class sapLibrary_2_0_a_7 {
 
 	// Version of the library
-	private $version = '2.0.a.6';
+	private $version = '2.0.a.7';
 
 	// A full URL to the library which is used to correctly link scripts and
 	// stylesheets.
